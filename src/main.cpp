7e60d968c20bc15237b9d5c79041fa3b439260d6
Migles <mlonniprado@gmail.com>
2024-08-07 20:33:39 -0300

    Fixed error because of c style string

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <fmt/core.h>
 #include <pugixml.hpp>
+#include <string>
+#include "cstring"
 
 using namespace std;
 
-int main(int argc, char** argv){
+int main(int argc, char *argv[]){
   
   bool fileFound = false;
+  std::string fileName;
 
-  pugi::xml_document doc;
   for (int i = 0; i < argc; ++i){
-    if (argv[i] == "-f"){
-      fmt::print("{} \n", argv[i+1]);
-      fileFound = true;
+    if (strcmp(argv[i],"-f") == 0){
+      if (argv[i+1] != nullptr){
+        fmt::print("{} \n", argv[i+1]);
+        fileFound = true;
+        fileName = argv[i+1];
+      }
+      else {
+        fmt::print("xml file not passed or passed incorectly");
+        return -1;
+      }
     }
   }
   if (!fileFound){
     fmt::print("Xml file hasnt been found, use -f file_name when executing file in command line");
     return -1;
   }
-  pugi::xml_parse_result data = doc.load_file(argv[1]);
-	fmt::print("Hello Git \n");
+
+  pugi::xml_document doc;
+  pugi::xml_parse_result data = doc.load_file(fileName.c_str());
+	fmt::print("Size of data: {} \n", sizeof(data));
 	return 0;
 }
