fbda7a69806871554fd313ccae59574f14395819
Migles <mlonniprado@gmail.com>
2024-08-07 20:09:14 -0300

    Reads file from command line flag -f

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,21 @@
 using namespace std;
 
 int main(int argc, char** argv){
+  
+  bool fileFound = false;
+
   pugi::xml_document doc;
-  pugi::parse_result data = doc.load_file()
+  for (int i = 0; i < argc; ++i){
+    if (argv[i] == "-f"){
+      fmt::print("{} \n", argv[i+1]);
+      fileFound = true;
+    }
+  }
+  if (!fileFound){
+    fmt::print("Xml file hasnt been found, use -f file_name when executing file in command line");
+    return -1;
+  }
+  pugi::xml_parse_result data = doc.load_file(argv[1]);
 	fmt::print("Hello Git \n");
 	return 0;
 }
