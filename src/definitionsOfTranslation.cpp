e9a2ee4dd87be1ab55b770c83fb5928989f4410a
Migles <mlonniprado@gmail.com>
2024-08-07 18:21:05 -0300

    Addition of unordered-map and changes on cmake

--- src/definitionsOfTranslation.cpp
+++ src/definitionsOfTranslation.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 #include <functional>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include <ankerl::unordered_dense>
+
+
+using json = nlohmann::json;
+
+void initialization(){
+  return; 
+}
+
