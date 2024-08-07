d52ba16385933a5d05cc3fc20fdbd4f11d5edcdc
Migles <mlonniprado@gmail.com>
2024-08-07 19:41:34 -0300

    Program takes command line arguments and xml lib is imported into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <fmt/core.h>
+#include <pugixml.hpp>
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+  pugi::xml_document doc;
+  pugi::parse_result data = doc.load_file()
 	fmt::print("Hello Git \n");
 	return 0;
 }
