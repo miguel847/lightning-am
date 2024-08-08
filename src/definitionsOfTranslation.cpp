b1969e5f11a51d163614a67b69d342346a842770
Migles <mlonniprado@gmail.com>
2024-08-08 19:28:52 -0300

    Basic infrestructure of translator object and translation functions function pointer defintion

--- src/definitionsOfTranslation.cpp
+++ src/definitionsOfTranslation.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
-#include <functional>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <memory>
 #include <nlohmann/json.hpp>
 #include <ankerl/unordered_dense.h>
+#include <pugixml.hpp>
 
 
 using json = nlohmann::json;
+typedef void (*func_ptr)(pugi::xml_node&, json&, std::vector<std::string>);
 
-void initialization(){
+void oscilate(pugi::xml_node& effect, json& translatedDoc, std::vector<std::string> plugins ){
   return; 
 }
+
+
+typedef struct translator{
+  ankerl::unordered_dense::map<std::string, func_ptr> dictionary;
+  std::unique_ptr<json> translatedDoc;
+
+  void initialization(){
+    translatedDoc = std::make_unique<json>();
+  }
+
+} translator;
 
